cba8d054d8e5170a1ecb5ce9a9d13d256242113b
Ryhor Krs <krasniakovg01@gmail.com>
2021-10-12 14:22:43 +0300

    created HomeComponent, SignInComponent and added some styles for AppComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { PostsComponent } from './posts/posts.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    HomeComponent,
+    PostsComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent, PostsComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
