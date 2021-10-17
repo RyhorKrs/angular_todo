442d364f6457563b037b904e25fa2a90226a0dd6
Ryhor Krs <krasniakovg01@gmail.com>
2021-10-17 19:39:49 +0300

    added progress bar and styles for password strength template

--- src/modules/app/sign-up/password-strength/password-strength.component.ts
+++ src/modules/app/sign-up/password-strength/password-strength.component.ts
@@ -13,4 +13,25 @@ export class PasswordStrengthComponent{
   public numberMatch: boolean = false;
   public symbolMatch: boolean = false;
   public lengthMatch: boolean = false;
+
+  /*private checkPasswordStrength(password: string): void {
+    this.passwordStrength =
+      Number(this.uppercaseMatch(password)) +
+      Number(this.lowercaseMatch(password)) +
+      Number(this.numberMatch(password)) +
+      Number(this.symbolMatch(password)) +
+      Number(this.lengthMatch(password));
+  }*/
+
+  /* REGEX 
+  export const REGEX = {
+    LINK: /^[a-z0-9\-_+:/]+\.[a-z0-9. \-_+]+$/i,
+    EMAIL: /^[a-z0-9.\-_+]+@[a-z0-9\-_+]+\.[a-z0-9. \-_+]+$/i,
+    PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+    UPPER_CASE: /(?=.*[A-Z])/,
+    LOWER_CASE: /(?=.*[a-z])/,
+    NINE_CHARACTERS: /[a-zA-Z\d@$#!%*?&^()-=_+]{9,}/,
+    NUMBER: /(?=.*\d)/,
+    SPECIAL_SYMBOL: /[!@#$%^&*(),.?":{}|<>]/
+  };*/
 }
